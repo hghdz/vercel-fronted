4eeb01fc1dbd632a6d6e07a0a200eb4b5d700581
엄한슬 <hghdz@naver.com>
2025-05-17 03:28:38 +0900

    feat: add full speaking slider with real data

--- pages/speaking-slider.tsx
+++ pages/speaking-slider.tsx
@@ -1,9 +1,10 @@
-// pages/speaking-slider.tsx
+import dynamic from "next/dynamic"
+
+const DynamicSpeakingSliderApp = dynamic(
+  () => import("../components/SpeakingSliderApp"),
+  { ssr: false }
+)
 
 export default function SpeakingSliderPage() {
-  return (
-    <div style={{ fontSize: "24px", padding: "2rem", textAlign: "center" }}>
-      ✅ 말하기 연습 테스트 화면입니다!
-    </div>
-  )
+  return <DynamicSpeakingSliderApp />
 }
