ef58acbe5bfd08cd911264252b3e6033624360ab
엄한슬 <hghdz@naver.com>
2025-05-18 01:15:43 +0900

    fix: disable SSR for speaking-slider page to avoid invalid-api-key build error

--- pages/speaking-slider.tsx
+++ pages/speaking-slider.tsx
@@ -1,17 +1,6 @@
 // pages/speaking-slider.tsx
 "use client"
-
-import dynamic from "next/dynamic"
-import React from "react"
-
-// SpeakingSliderApp 을 클라이언트 전용으로 동적 로드 (SSR 비활성화)
-const SpeakingSliderApp = dynamic(
-  () => import("../components/SpeakingSliderApp"),
-  {
-    ssr: false,
-    loading: () => <p style={{ textAlign: "center" }}>🔄 슬라이더 로딩 중...</p>,
-  }
-)
+import SpeakingSliderApp from "../components/SpeakingSliderApp"
 
 export default function SpeakingSliderPage() {
   return <SpeakingSliderApp />
