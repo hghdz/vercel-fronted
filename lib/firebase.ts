ab03706dfc89fe7aa364e3a1f4e0a465b29c2103
엄한슬 <hghdz@naver.com>
2025-05-17 13:10:18 +0900

    chore: add firebase initialization file

---
+++ lib/firebase.ts
@@ -0,0 +1,15 @@
+// lib/firebase.ts
+import { initializeApp, getApps } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: 'YOUR_API_KEY',
+  authDomain: 'YOUR_AUTH_DOMAIN',
+  projectId: 'YOUR_PROJECT_ID',
+  storageBucket: 'YOUR_STORAGE_BUCKET',
+  messagingSenderId: 'YOUR_SENDER_ID',
+  appId: 'YOUR_APP_ID',
+}
+
+if (!getApps().length) {
+  initializeApp(firebaseConfig)
+}
