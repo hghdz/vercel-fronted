378acf627da26c8a2443278d61251cc46b972547
엄한슬 <hghdz@naver.com>
2025-05-17 19:52:55 +0900

    fix: import navbar with correct lowercase path

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
-import Navbar from '../components/Navbar'
+import Navbar from '../components/navbar'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
       <body>
-        <Navbar />
+        <navbar />
         {children}
       </body>
     </html>
