7f99bbac83ec83dd06b2b77fd17e16eda6ce165e
엄한슬 <hghdz@naver.com>
2025-05-19 02:15:53 +0900

    feat: add speaking values page

---
+++ pages/speaking/values.tsx
@@ -0,0 +1,23 @@
+// pages/speaking/values.tsx
+
+import React from 'react'
+import { useRouter } from 'next/router'
+import ValuesSpeakingSliderApp from '../../components/ValuesSpeakingSliderApp'
+
+export default function SpeakingValuesPage() {
+  const { query } = useRouter()
+  const emailParam = query.email
+  const email = Array.isArray(emailParam) ? emailParam[0] : emailParam
+
+  // 이메일 정보가 없으면 안내
+  if (!email) {
+    return <div style={{ padding: 24 }}>이메일 정보가 없습니다.</div>
+  }
+
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>가치관 말하기 연습</h1>
+      <ValuesSpeakingSliderApp email={email} />
+    </div>
+  )
+}
