78e96c0f58da1f54150ac607824fcb7c991ec4b8
엄한슬 <hghdz@naver.com>
2025-05-24 17:38:03 +0900

    메시지 입력

--- pages/api/get-strengths.ts
+++ pages/api/get-strengths.ts
@@ -27,7 +27,7 @@ export default async function handler(
 
   try {
     const client = await clientPromise;
-    const db = client.db();
+    const db = client.db(MENG);
     const record = await db
       .collection('strengths')
       .findOne({ email: email.toLowerCase() });
